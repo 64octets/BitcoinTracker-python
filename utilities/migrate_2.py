7f67955dab5501830cccf983acae1e59df0b4390
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2014-03-20 12:19:01 -0500

    Adds a migrations script to calculated weighted_average for prices. Adds a utilties module to carry out said calculation.

---
+++ utilities/migrate_2.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+#
+#
+# Copyright 2014 Abid Hasan Mujtaba
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Author: Abid H. Mujtaba
+# Date: 2014-03-20
+#
+# This script reads buy and price data from the database, calculates the weighted average and stores it in the database.
+
+
+import sqlite3
+
+from weighted_average import weighted_running_average, NUM_WEIGHING_SAMPLES, WEIGHING_FUNCTION
+
+
+if __name__ == '__main__':
+
+    conn = sqlite3.connect('../data.db')
+    cursor = conn.cursor()
+
+    s_time = []
+    s_buy = []
+    s_sell = []
+
+    for values in cursor.execute('''SELECT "time", "buy", "sell" FROM "prices"'''):
+
+        s_time.append( values[0] )
+        s_buy.append( values[1] )
+        s_sell.append( values[2] )
+
+    print("Number of records to be processed: " + str(len(s_time)))
+
+    wa_buy = weighted_running_average( s_buy, NUM_WEIGHING_SAMPLES, WEIGHING_FUNCTION )
+    wa_sell = weighted_running_average( s_sell, NUM_WEIGHING_SAMPLES, WEIGHING_FUNCTION )
+
+    print("Weighted Running Averages calculated.\nInserting in to database...")
+
+
+    for ii in range(len(s_time)):
+
+        time = s_time[ii]
+        wbuy = wa_buy[ii]
+        wsell = wa_sell[ii]
+
+        cursor.execute('''UPDATE "prices" SET "wa_buy" = ?, "wa_sell" = ? WHERE "time" = ?''', (wbuy, wsell, time))
+
+    conn.commit()       # Commit changes
+    conn.close()        # Close connection
+
+    print("Done")
---
+++ utilities/weighted_average.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+#
+#
+# Copyright 2014 Abid Hasan Mujtaba
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Author: Abid H. Mujtaba
+# Date: 2014-03-20
+#
+# This module implements utilities for calculating the weighted average of a data series.
+
+
+NUM_WEIGHING_SAMPLES = 1 * 12      # Define the weighted sum sampling window to be 1 hour: i.e. the number of 5 min intervals in 1 hour
+
+WEIGHING_FUNCTION = lambda x: (NUM_WEIGHING_SAMPLES - x)           # We use a linear weighing function. x = 0 is the end-value
+                                                                   # and as x increases we are looking at successively older values
+
+
+def weighted_running_average(series, N, weighing_function = None):
+    """
+    Calculates the Weighted Running Average of a series over the last N values using the specified weighing_funciton.
+    """
+
+    if not weighing_function:       # No weighing function provided so we specify the default one
+
+        weighing_function = lambda x: 1       # Standard mean over N values. NOTE: If you set N = 1 you get the exact value back
+
+    weighted_series = []
+
+    for ii in range(len(series)):
+
+        sum = 0
+        denom = 0
+
+        for jj in range(N):
+
+            if (ii - jj) < 0:
+
+                break
+
+            sum += series[ii - jj] * weighing_function(jj)       # Multiply value by weight calculated accordingly to distance from main value
+            denom += weighing_function(jj)
+
+        weighted_series.append( round( sum / float(denom), 2) )     # Append the calculated weighted sum (rounded to 2 decimal places) to the output series
+
+    return weighted_series
